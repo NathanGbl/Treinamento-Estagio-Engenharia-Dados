503270a75b0d65cae6ef9cd2662d4d9fd7675b01
Nathan Gabriel da Fonseca Leite <nathangabriel.f.leite@gmail.com>
2025-06-28 21:45:02 -0300

    Projeto quase concluido. Falta resolver erro no merge com a dm_vendedor e validar os dados. fiz também um código sql para criar a dm_tempo.

---
+++ main.bat
@@ -0,0 +1,2 @@
+@echo off
+python "C:\Users\natha\source\repos\projeto\Treinamento-Estagio-Engenharia-Dados\main.py"
--- main.py
+++ main.py
@@ -1,12 +1,4 @@
 import pandas as pd
 
-def normaliza_seguradora(filepath: str):
-    df = None
-    try:
-        df = pd.read_excel(filepath)
-        print(df.head())
-    except Exception as e:
-        raise(f"Erro ao ler o arquivo: {e}")
-    clientes = df[["CLIENTE CNPJ", "CLIENTE RAZÃO SOCIAL", "CLIENTE UF"]]
-    vendedores = df[["VENDEDOR", "CÓDIGO VENDEDEOR", "SEGURADORA"]]
-    venda = df[["CLIENTE CNPJ", "CÓDIGO VENDEDEOR", "Cod_Nota", "Data", "Modalidade"]]
+df = pd.read_excel('SeguradorasNormalização 1.xls', dtype=str)
+df.to_excel("SeguradorasNormalização 1.xlsx", index=False)
