bd6211b24904254591daa8dd4288c40d7b49ed15
Nathan Gabriel da Fonseca Leite <nathangabriel.f.leite@gmail.com>
2025-07-14 16:54:04 -0300

    Campo DIA do excel xlsx ao subir pra o banco está como nulo.

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pandas as pd
 
 df = pd.read_excel('SeguradorasNormalização 1.xls', dtype=str)
+
+df['DIA'] = df['Data'].str.strip().str.split().str[1].astype(str)
+
 df.to_excel("SeguradorasNormalização 1.xlsx", index=False)
